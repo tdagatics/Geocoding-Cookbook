91f483801e5be9b5b43507fc39b634e65b989f84
Anthony Dagati <tdagati.cs@gmail.com>
2014-11-07 09:19:48 -0800

    Initial Commit

---
+++ Recipe 31 Geocoding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Recipe 31 Geocoding
+//
+//  Created by Anthony Dagati on 11/7/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Recipe 31 Geocoding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Recipe 31 Geocoding
+//
+//  Created by Anthony Dagati on 11/7/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
